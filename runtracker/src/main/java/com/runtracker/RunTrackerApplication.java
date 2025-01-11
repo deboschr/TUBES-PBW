4aa98a95905f96a580a03f9a00eec166417ca926
deboschr <debospencher@gmail.com>
2025-01-11 07:57:00 +0700

    asa

--- runtracker/src/main/java/com/runtracker/RunTrackerApplication.java
+++ runtracker/src/main/java/com/runtracker/RunTrackerApplication.java
@@ -1,4 +1,4 @@
-package com.runtracker.runtracker;
+package com.runtracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
